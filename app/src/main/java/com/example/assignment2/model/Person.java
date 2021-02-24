b8452190b7fb45baba5ef4d562ab7681ceb712af
Jamessngg <55782147+JamesSng@users.noreply.github.com>
2021-02-24 21:05:40 +0800

    Added password info to person class

--- app/src/main/java/com/example/assignment2/model/Person.java
+++ app/src/main/java/com/example/assignment2/model/Person.java
@@ -3,6 +3,8 @@ package com.example.assignment2.model;
 public class Person {
 
     private String icNumber, name;
+    private String password;
+    private boolean passwordSet;
     private int vaccineStatus;
     public static final int UNVACCINATED = 0;
     public static final int FIRST_SHOT = 1;
@@ -10,13 +12,18 @@ public class Person {
     public static final int RECOVERED = 3;
 
     public Person(String icNumber, String name){
-        this(icNumber, name, UNVACCINATED);
+        this.icNumber = icNumber;
+        this.name = name;
+        this.vaccineStatus = UNVACCINATED;
+        this.passwordSet = false;
     }
 
-    public Person(String icNumber, String name, int vaccineStatus){
+    public Person(String icNumber, String name, int vaccineStatus, String password){
         this.icNumber = icNumber;
         this.name = name;
         this.vaccineStatus = vaccineStatus;
+        this.password = password;
+        this.passwordSet = true;
     }
 
     public String getIcNumber() {
@@ -25,6 +32,20 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    public void setPassword(String password){
+        //to implement password checking
+        this.password = password;
+        passwordSet = true;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public boolean isPasswordSet(){
+        return passwordSet;
     }
 
     public int getVaccineStatus() {
