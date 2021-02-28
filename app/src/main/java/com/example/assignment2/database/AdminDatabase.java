f862b86eeb2dc9a384d0ce126395a5730a7fb544
Jamessngg <55782147+JamesSng@users.noreply.github.com>
2021-02-28 18:32:08 +0800

    Added AdminDatabase.java

---
+++ app/src/main/java/com/example/assignment2/database/AdminDatabase.java
@@ -0,0 +1,41 @@
+package com.example.assignment2.database;
+
+import com.example.assignment2.model.Admin;
+
+import java.util.ArrayList;
+
+public class AdminDatabase {
+
+    ArrayList<Admin> admins;
+    Admin currentAdmin;
+
+    public static int LOGIN_SUCCESSFUL = 0;
+    public static int INVALID_NAME = 1;
+    public static int INVALID_PASSWORD = 2;
+
+    public int login(String username, String password){
+        for (Admin admin: admins){
+            if (admin.getUsername().equals(username)){
+                if (admin.getPassword().equals(password)){
+                    currentAdmin = admin;
+                    return LOGIN_SUCCESSFUL;
+                }
+                else return INVALID_PASSWORD;
+            }
+        }
+        return INVALID_NAME;
+    }
+
+    public void logout(){
+        currentAdmin = null;
+    }
+
+    public void loadDB(String str){
+        String[] adminArr = str.split(";");
+        for (String admin : adminArr){
+            String[] details = admin.split(",");
+            Admin a = new Admin(details[0], details[1]);
+            admins.add(a);
+        }
+    }
+}
